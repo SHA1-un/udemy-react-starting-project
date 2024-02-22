0dda148b723638e8c33d79ac65723c9f94eb420b
Shaun <shaun@gatekeeper.technology>
2024-02-22 21:16:10 +0200

    Starting project completed

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,8 +1,12 @@
-export default function Tabs({children, buttons}) {
+export default function Tabs({children, buttons, ButtonsContainer = 'menu'}) {
     return (
         <>
-            <menu>{buttons}</menu>
+            <ButtonsContainer>{buttons}</ButtonsContainer>
             {children}
         </>
     );
 }
+
+// Note: ButtonsContainer must start with a capital letter since it must be rendered as a
+// custom component. Using buttonsContainer in your JSX will not work since React will 
+// look for a built in component with that name.
